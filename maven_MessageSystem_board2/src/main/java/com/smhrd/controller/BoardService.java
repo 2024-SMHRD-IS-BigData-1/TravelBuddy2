a02c59d4a20199d4bd740f317ee36cb3b4bc0276
한현 <138780030+jojo12425@users.noreply.github.com>
2024-04-22 11:22:46 +0900

    한현 pull커밋

--- maven_MessageSystem_board2/src/main/java/com/smhrd/controller/BoardService.java
+++ maven_MessageSystem_board2/src/main/java/com/smhrd/controller/BoardService.java
@@ -7,57 +7,42 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.oreilly.servlet.MultipartRequest;
-import com.oreilly.servlet.multipart.DefaultFileRenamePolicy;
 import com.smhrd.model.Board;
 import com.smhrd.model.BoardDAO;
 
 @WebServlet("/BoardService")
 public class BoardService extends HttpServlet {
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	
-		// post 방식 인고딩
-		request.setCharacterEncoding("UTF-8");
-		
-		//파일업로드를 위한 변수 설정
-		// 1. request 객체
-		// 2. 파일을 저장할 경로(문자열)->상대경로
-		// 요청에 관련된 정보이기 떄문에 request객체을 사용해서 상대경로 알아보기
-		String path = request.getServletContext().getRealPath("./img");
-		System.out.println(path);
-		
-		// 3. 파일 최대 크기(정수)
-		int maxSize = 10*1024*1024; // 10MB
-		
-		// 4. 인코딩 타입 (문자열)
-		String encoding = "UTF-8";
-		
-		// 5. 파일 이름 중복제거
-		 DefaultFileRenamePolicy rename = new DefaultFileRenamePolicy();
-		 
-		 MultipartRequest multi = new MultipartRequest(request, path, maxSize, encoding, rename);
-		 
-		 // 데이터 가져오기
-		 String title = multi.getParameter("title");
-		 String writer = multi.getParameter("writer");
-		 String filename = multi.getFilesystemName("filename");
-		 String content = multi.getParameter("content");
-		 
-		// System.out.println(title + writer + filename + content);
-		 
-		Board board = new Board(title, writer, filename,content);
-		
-		int cnt = new BoardDAO().insertBoard(board);
-		
-		if(cnt > 0 ) {
-			System.out.println("파일 업로드 성공");
-		}else {
-			System.out.println("파일 업로드 실패");
-		}
-		
-		
-		 
-		 
-	}
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    
+        // post 방식 인코딩
+        request.setCharacterEncoding("UTF-8");
+        
+        // 데이터 가져오기
+        String place_name = request.getParameter("place_name");
+        String lat = request.getParameter("lat");
+        String lng = request.getParameter("lng");
+        String title = request.getParameter("title");
+        String content = request.getParameter("content");
+        String travel_dt = request.getParameter("travel_dt");
+        String mem_id = request.getParameter("mem_id");
+        System.out.println("place_name: " + place_name);
+        System.out.println("lat: " + lat);
+        System.out.println("lng: " + lng);
+        System.out.println("title: " + title);
+        System.out.println("content: " + content);
+        System.out.println("travel_dt: " + travel_dt);
+        System.out.println("mem_id: " + mem_id);
 
+        // 생성자 수정
+        Board board = new Board(0, place_name, lat, lng, title, content, travel_dt, mem_id);
+        
+        int cnt = new BoardDAO().insertBoard(board);
+        
+        
+        if (cnt > 0) {
+            System.out.println("게시글 등록 성공");
+        } else {
+            System.out.println("게시글 등록 실패");
+        }
+    }
 }
--- maven_MessageSystem_board2/src/main/java/com/smhrd/model/Board.java
+++ maven_MessageSystem_board2/src/main/java/com/smhrd/model/Board.java
@@ -3,26 +3,27 @@ package com.smhrd.model;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.NonNull;
-import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
+//수정된 Board 모델 클래스
 @AllArgsConstructor
-@RequiredArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
 @ToString
-
 public class Board {
 
-	
-	private int num;
-	@NonNull private String title;
-	@NonNull private String writer;
-	@NonNull private String filename;
-	@NonNull private String content;
-	private String b_date;
+ private int buddy_idx;
+ private String place_name;
+ private String lat;
+ private String lng;
+ private String title;
+ private String content;
+ private String travel_dt;
+ private String mem_id;
+
+ // place_name 속성에 대한 getter 메서드 추가
+
 
 }
