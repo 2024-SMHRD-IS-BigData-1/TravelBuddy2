4c6ba3a25544888a1722f150a1b238d470a19247
한현 <138780030+jojo12425@users.noreply.github.com>
2024-04-23 16:59:39 +0900

    한현 커밋

--- TravelBuddy/src/main/java/com/smhrd/controller/LoginService.java
+++ TravelBuddy/src/main/java/com/smhrd/controller/LoginService.java
@@ -1,40 +1,37 @@
 package com.smhrd.controller;
 
-import java.io.IOException;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import com.smhrd.frontcontroller.Command;
 import com.smhrd.model.Member;
 import com.smhrd.model.MemberDAO;
 
-@WebServlet("/LoginService")
-public class LoginService extends HttpServlet {
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		System.out.println("로그인 페이지 입성!");
-		
-		request.setCharacterEncoding("UTF-8");
-		
-		String id = request.getParameter("id");
+public class LoginService implements Command{
+
+	public String execute(HttpServletRequest request, HttpServletResponse response) {
+		// 로그인 기능
+		String email = request.getParameter("email");
 		String pw = request.getParameter("pw");
-		
-		Member member = new Member(id,pw);
-		
+
+		// System.out.println(email + pw);
+
+		Member member = new Member(email, pw);
+
 		Member loginMember = new MemberDAO().login(member);
-	
-		if(loginMember != null) {
-			System.out.println("로그인 성공");
+
+		if (loginMember != null) {
+			System.out.println("로그인 성공!");
 			HttpSession session = request.getSession();
 			session.setAttribute("loginMember", loginMember);
-		}else {
-			System.out.println("로그인실패");
+		} else {
+			System.out.println("로그인 실패..");
 		}
-		
-		response.sendRedirect("Login.jsp");
+
+	//	response.sendRedirect("Main.jsp");
+
+		return "Main.jsp";
 	}
 
 }
